fa478223d3d40112187d5f8196369f381584fd27
Dataprove <79802015+Dataprove@users.noreply.github.com>
2021-09-15 21:03:21 +0100

    Update DataProVe-prototype.py

--- Artifacts/DataProVe-prototype.py
+++ Artifacts/DataProVe-prototype.py
@@ -17,6 +17,20 @@ page of the tool (https://github.com/Dataprove/Dataprovetool/), which will be ma
 Python was chosen for the prototype due to its large community and great support (inc. GUI libraries). 
 Possible changes also include switching to other programming langauge to improve efficiency. 
 
+License: 
+
+Copyright (C) 2021  Vinh Thong Ta
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
 """
 
 
